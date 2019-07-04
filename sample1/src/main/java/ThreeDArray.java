d1c581080ab7aa729448c5aad1eb814f32bfce37
dpavan <dpavan@DIN78000341.corp.capgemini.com>
2019-07-04 12:23:27 +0530

    done

---
+++ sample1/src/main/java/ThreeDArray.java
@@ -0,0 +1,12 @@
+
+
+public class ThreeDArray {
+public static void main(String [] args)
+{
+	int [] [] [] arr= {{{1,2},{3,4}},{{6,7},{8,9}}};
+    for (int i=0;i<2;i++)
+    	for(int j=0;j<2;j++)
+    	for(int k=0;k<2;k++)
+   System.out.println("arr["+i+"]["+j+"]["+k+"]="+arr[i][j][k]);
+}
+}
