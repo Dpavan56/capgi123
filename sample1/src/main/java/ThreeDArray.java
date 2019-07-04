92efe2dd19c420de2451a32cfb0a4aba8b538f4a
dpavan <dpavan@DIN78000341.corp.capgemini.com>
2019-07-04 13:07:18 +0530

    changes done at palindrome

--- sample1/src/main/java/ThreeDArray.java
+++ sample1/src/main/java/ThreeDArray.java
@@ -8,5 +8,6 @@ public static void main(String [] args)
     	for(int j=0;j<2;j++)
     	for(int k=0;k<2;k++)
    System.out.println("arr["+i+"]["+j+"]["+k+"]="+arr[i][j][k]);
+    System.out.println("changes made");
 }
 }
